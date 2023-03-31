edc1aab056dce66ea54c18540c72afc402e52f30
Ihor <kokhan.igorek05@gmail.com>
2023-03-31 15:41:38 +0300

    Task1 completed

--- Lection2/Task1/main.py
+++ Lection2/Task1/main.py
@@ -1,19 +1,19 @@
-# Ask the user to enter the number of elements
+# Asking the user to enter the number of elements
 n = int(input("Введите количество элементов: "))
 
 # Create an empty list to store items
 elements = []
 
-# Ask the user to enter each element and add it to the list
+# Asking the user to enter each item and adding it to the list
 for i in range(n):
     element = input("Введите элемент: ")
     elements.append(element)
 
-# Ask the user to enter the element to be found
+# Asking the user to enter the element to be found
 search_element = input("Введите элемент для поиска: ")
 
-# Count the number of occurrences of the element in the list
+# Count the number of occurrences of an element in a list
 count = elements.count(search_element)
 
-# Output the result
+# Output result
 print("Количество элемента", search_element, "в списке:", count)
